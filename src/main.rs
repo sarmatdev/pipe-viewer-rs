ebe7a7ee61557458f70dd1b27cc84d2c9d656121
sarmat <skhachirov12@gmail.com>
2023-04-30 16:40:21 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::io::{self, Read, Write};
+
+const CHUNK_SIZE: usize = 18 * 1024;
+
+fn main() {
+  let silent = env::var("PV_SILENT").unwrap_or_default().is_empty();
+  dbg!(silent);
+  let mut total_bytes = 0;
+  loop {
+    let mut buffer = [0; CHUNK_SIZE];
+    let num_read = match io::stdin().read(&mut buffer) {
+      Ok(0) => break,
+      Ok(x) => x,
+      Err(_) => break,
+    };
+    dbg!(total_bytes += num_read);
+    io::stdout().write_all(&buffer[..num_read]).unwrap();
+  }
+
+  if dbg!(!silent) {
+    eprintln!("total_bytes: {}", total_bytes);
+  }
+}
