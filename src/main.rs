bbe96688052b72498f1613b0c68c1b2321dcae50
sarmat <skhachirov12@gmail.com>
2023-04-30 16:56:45 -0500

    feat: better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
 use std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, ErrorKind, Read, Result, Write};
 
 const CHUNK_SIZE: usize = 18 * 1024;
 
-fn main() {
-  let silent = env::var("PV_SILENT").unwrap_or_default().is_empty();
-  dbg!(silent);
+fn main() -> Result<()> {
+  let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
   let mut total_bytes = 0;
+  let mut buffer = [0; CHUNK_SIZE];
   loop {
-    let mut buffer = [0; CHUNK_SIZE];
     let num_read = match io::stdin().read(&mut buffer) {
       Ok(0) => break,
       Ok(x) => x,
       Err(_) => break,
     };
-    dbg!(total_bytes += num_read);
-    io::stdout().write_all(&buffer[..num_read]).unwrap();
+    total_bytes += num_read;
+    if !silent {
+      eprint!("\r{}", total_bytes);
+    }
+    if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
+      if e.kind() == ErrorKind::BrokenPipe {
+        break;
+      }
+      return Err(e);
+    };
   }
 
-  if dbg!(!silent) {
-    eprintln!("total_bytes: {}", total_bytes);
+  if !silent {
+    eprintln!("\r{}", total_bytes);
   }
+  Ok(())
 }
